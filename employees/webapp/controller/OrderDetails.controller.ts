643317334b5062d68a893f7c8cd457c11c4d4ec9
Jorge Soto <jlscsi@hotmail.com>
2026-03-04 17:23:28 +0000

    04/03/2026

--- employees/webapp/controller/OrderDetails.controller.ts
+++ employees/webapp/controller/OrderDetails.controller.ts
@@ -9,6 +9,10 @@ import ResourceBundle from "sap/base/i18n/ResourceBundle";
 import JSONModel from "sap/ui/model/json/JSONModel";
 import ODataListBinding from "sap/ui/model/odata/v2/ODataListBinding";
 import Filter from "sap/ui/model/Filter";
+import UploadSet, { UploadSet$AfterItemRemovedEvent, UploadSet$BeforeUploadStartsEvent, UploadSet$UploadCompletedEvent } from "sap/m/upload/UploadSet";
+import UploadSetItem, { UploadSetItem$OpenPressedEvent } from "sap/m/upload/UploadSetItem";
+import ODataModel from "sap/ui/model/odata/v2/ODataModel";
+import Item from "sap/ui/core/Item";
 
 /**
  * @namespace com.logaligroup.employees.controller
@@ -35,7 +39,8 @@ export default class OrderDetails extends BaseController {
             model: 'northwind',
             events: {
                 change: () => {
-                 this.read();   
+                 this.read();
+                 this.searchFiles();   
                 },
                 dataRequest: () => {
                     view.setBusy(true);
@@ -107,6 +112,82 @@ export default class OrderDetails extends BaseController {
 
     public onRefreshPress(){
         this.read();
+        ;
     }
-   
+
+    public onBeforeUpload(event: UploadSet$BeforeUploadStartsEvent): void{
+        const item = event.getParameter("item") as UploadSetItem;
+        const oUtils = new Utils(this);
+        const context = this.getView()?.getBindingContext("northwind");
+        const model = this.getOwnerComponent()?.getModel("zinvoices") as ODataModel;
+        const token = model.getSecurityToken();
+        const filename = item.getFileName();
+        const mediaType = item.getMediaType();
+        const orderId = context?.getProperty("OrderID")
+        const sapId = oUtils.getEmail();
+        const employeeId = context?.getProperty("EmployeeID");
+
+        const headerToken = new Item ({
+            key: "x-csrf-token",
+            text: token
+        });
+        const headerSlug = new Item ({
+            key: "slug",
+            text: `${orderId};${sapId};${employeeId};${filename};${mediaType}`
+        });
+        console.log({filename,mediaType,token,orderId,sapId,employeeId}); 
+        item.addHeaderField(headerToken);
+        item.addHeaderField(headerSlug);
+    }
+
+    public onUploadCompleted( event : UploadSet$UploadCompletedEvent) : void {
+        const uploadSet = event.getSource();
+         uploadSet.getBinding("items")?.refresh();
+    }
+
+    private searchFiles() : void{
+        const oUtils = new Utils(this);
+        const context = this.getView()?.getBindingContext("northwind");
+        const orderId = context?.getProperty("OrderID")
+        const sapId = oUtils.getEmail();
+        const employeeId = context?.getProperty("EmployeeID");
+        //lo que sigue lo carga de forma dinamica y no estatica como en la vista
+        const uploadSet = this.byId("upload") as UploadSet;
+              uploadSet.bindAggregation("items",{
+                path: 'zinvoices>/FilesSet',
+                filters:[
+                    new Filter("OrderId","EQ", orderId),
+                    new Filter("SapId","EQ", sapId),
+                    new Filter("EmployeeId","EQ", employeeId)
+                ],
+                //template siempre se usa para carga dinamica
+                template: new UploadSetItem({
+                    fileName: '{zinvoices>FileName}',
+                    mediaType:'{zinvoices>MimeType}',
+                    visibleEdit:false,
+                    visibleRemove:true,
+                    url:'hola',
+                    openPressed: this.download.bind(this)
+                })
+              })
+    }
+
+    private download (event : UploadSetItem$OpenPressedEvent): void{
+        const item = event.getSource() as UploadSetItem;
+        const context = item.getBindingContext("zinvoices") as Context;
+        const path = context.getPath();
+        console.log(path)
+        ///sap/opu/odata/sap/YSAPUI5_SRV_01/FilesSet(AttId='0709',OrderId='010258',SapId='c25c385%40gmail.com',EmployeeId='0001')/$value
+        const url=`/sap/opu/odata/sap/YSAPUI5_SRV_01${path}/$value`
+        item.setUrl(url);
+    }
+    public async onAfterRemoved( event: UploadSet$AfterItemRemovedEvent) : Promise<void>{
+        const item = event.getParameter("item") as UploadSetItem;
+        const context = item.getBindingContext("zinvoices") as Context;
+        const path = context.getPath();
+        
+        const oUtils = new Utils(this);
+        await oUtils.crud('delete', new JSONModel({path: path}));
+        item.getBinding("items")?.refresh();
+    }
 }
